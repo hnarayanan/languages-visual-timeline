56fd1469430062cac12a7495386eac6f79e91aa0
Harish Narayanan <hnarayanan@gmail.com>
2023-01-14 20:07:54 +0000

    Rewrite the generator script to work with jinja templates for future flexibility

--- generate.py
+++ generate.py
@@ -1,39 +1,31 @@
-# This script generates a file called languages.dot, which can be
-# converted to a pretty graphic file using Graphviz:
-#
-# dot -Tsvg languages.dot -o languages.svg
+"""
+Running this script generates a file called languages.dot, which can
+be converted to a pretty graphic file using Graphviz:
+
+$ dot -Tsvg languages.dot -o languages.svg
+"""
 
 import pandas as pd
+from jinja2 import Environment, FileSystemLoader
 
 
-DATA_URL = "https://en.wikipedia.org/wiki/Timeline_of_programming_languages"
-INTERESTING_TABLES_START = 4
-INTERESTING_TABLES_END = 12
-FAKE_PREDECESSORS = ["none (unique language)", "—", "Predecessor(s)"]
-FAKE_NAMES = ["Name"]
-IMPLEMENTATION_SUFFIX = " (implementation)"
+def clean_string(string):
+    IMPLEMENTATION_SUFFIX = " (implementation)"
 
-FRONT_MATTER = """
-digraph languages {
-	fontname="Helvetica,Arial,sans-serif";
-	node [fontname="Helvetica,Arial,sans-serif"];
-	edge [fontname="Helvetica,Arial,sans-serif"];
-	node [color=lightblue2, style=filled];
-        rankdir="LR";
-"""
-END_MATTER = """
-}
-"""
-
-def transform(string):
     string = string.strip()
     string = string.replace(IMPLEMENTATION_SUFFIX, "")
     return string
 
-tables = pd.read_html(DATA_URL, header=0)
 
-with open("languages.dot", "w") as languages:
-    languages.write(FRONT_MATTER)
+def load_languages_data():
+    DATA_URL = "https://en.wikipedia.org/wiki/Timeline_of_programming_languages"
+    INTERESTING_TABLES_START = 4
+    INTERESTING_TABLES_END = 12
+    FAKE_PREDECESSORS = ["none (unique language)", "—", "Predecessor(s)"]
+    FAKE_NAMES = ["Name"]
+
+    languages = list()
+    tables = pd.read_html(DATA_URL)
 
     for table_idx in range(INTERESTING_TABLES_START, INTERESTING_TABLES_END + 1):
         table = tables[table_idx].fillna("")
@@ -42,16 +34,42 @@ with open("languages.dot", "w") as languages:
                 predecessors = str(language["Predecessor(s)"]).split(",")
                 for predecessor in predecessors:
                     if predecessor not in FAKE_PREDECESSORS:
-                        languages.write(
-                            '"{predecessor}" -> "{language}"\n'.format(
-                                predecessor=transform(predecessor), language=transform(language["Name"])
-                            )
+                        languages.append(
+                            {
+                                "predecessor": clean_string(predecessor),
+                                "name": clean_string(language["Name"]),
+                            }
                         )
                     else:
                         if language["Name"] not in FAKE_NAMES:
-                            languages.write('"{language}"\n'.format(language=transform(language["Name"])))
+                            languages.append(
+                                {
+                                    "name": clean_string(language["Name"]),
+                                }
+                            )
             else:
-                if language["Name"] not in FAKE_NAMES:                
-                    languages.write('"{language}"\n'.format(language=transform(language["Name"])))
+                if language["Name"] not in FAKE_NAMES:
+                    languages.append(
+                        {
+                            "name": clean_string(language["Name"]),
+                        }
+                    )
 
-    languages.write(END_MATTER)
+    return languages
+
+
+def load_languages_template():
+    loader = FileSystemLoader(searchpath="./templates/")
+    env = Environment(loader=loader)
+    template = env.get_template("languages.dot")
+    return template
+
+
+if __name__ == "__main__":
+
+    template = load_languages_template()
+    languages = load_languages_data()
+    rendered_template = template.render(languages=languages)
+
+    with open("languages.dot", "w", encoding="utf-8") as f:
+        f.write(rendered_template)
